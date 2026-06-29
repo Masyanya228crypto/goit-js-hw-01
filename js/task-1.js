0981b5c36460f3c1dce2d698a49247b07840087e
Maksim <maksimmoskalenko818@gmail.com>
2026-06-30 01:14:08 +0300

    3

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
 function makeTransaction (quantity, pricePerDroid) {
- 
+    const total = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${total} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
